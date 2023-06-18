26dedc45de3507681b92e72bc5b76ac04432a040
paukstelom <paukstelom@gmail.com>
2023-06-18 17:46:05 +0300

    Added ticket table

--- src/app/events/[eventId]/page.tsx
+++ src/app/events/[eventId]/page.tsx
@@ -2,6 +2,7 @@ import router from 'next/navigation'
 import { Sponsor } from 'sponsorbook/components/sponsor'
 import { getOneEvent, getOneSponsor } from 'sponsorbook/clients/sponsorbook'
 import { Eventer } from 'sponsorbook/components/eventCard'
+import TicketTable from 'sponsorbook/components/ticketTable'
 
 export default async function Event({
     params,
@@ -16,6 +17,18 @@ export default async function Event({
             <h1>Name: {event.name}</h1>
             <h4>Status: {event.status}</h4>
             <h4>Desciprtion {event.description}</h4>
+            <h1>Tickets: </h1>
+            <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-container="body"
+                data-bs-toggle="popover"
+                data-bs-placement="bottom"
+                data-bs-content="Bottom popover"
+            >
+                Popover on bottom
+            </button>
+            <TicketTable tickets={[]} />
         </>
     )
 }
