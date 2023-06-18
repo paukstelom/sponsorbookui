3432f8149c3176e47e93af24494748c6e69683f9
paukstelom <paukstelom@gmail.com>
2023-06-18 23:16:53 +0300

    bruh

---
+++ src/components/sponsor copy.tsx
@@ -0,0 +1,85 @@
+'use client'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+
+import { Contact, Rating } from 'sponsorbook/app/sponsors/new/page'
+import { deleteOneSponsor } from 'sponsorbook/clients/sponsorbook'
+
+export type Sponsor = {
+    _id: string
+    companyNumber: string
+    contacts: Contact[]
+    website: string
+    rating: Rating
+    name: string
+    category: string
+    description: string
+}
+
+export type SponsorCardProps = {
+    sponsor: Sponsor
+}
+
+export default function SponsorLine({ sponsor }: SponsorCardProps) {
+    const router = useRouter()
+
+    const onSubmit = async () => {
+        await deleteOneSponsor(sponsor._id)
+
+        router.refresh()
+    }
+
+    return (
+        <>
+            <td className="text-center">{sponsor.name}</td>
+            <td className="text-center">{sponsor.category}</td>
+            <td className="text-center">
+                <span className="badge badge-success rounded-pill d-inline bg-success">
+                    Available
+                </span>
+            </td>
+            <td className="text-center">{sponsor.contacts[0].email}</td>
+            <td className="text-center">{sponsor.contacts[0].phone}</td>
+            <td className="text-center">🔥🔥🔥💩💩</td>
+            <td className="text-align-right">
+                <div className="dropdown">
+                    <button
+                        className="btn dropdown-toggle"
+                        type="button"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                    ></button>
+                    <ul className="dropdown-menu">
+                        <li>
+                            <a className="dropdown-item" href="#">
+                                Connect
+                            </a>
+                        </li>
+                        <li>
+                            <Link
+                                className="dropdown-item"
+                                href={`/sponsors/${sponsor._id}`}
+                            >
+                                View
+                            </Link>
+                        </li>
+                        <li>
+                            <a className="dropdown-item" href="#">
+                                Edit
+                            </a>
+                        </li>
+                        <li>
+                            <button
+                                className="dropdown-item"
+                                type="submit"
+                                onClick={onSubmit}
+                            >
+                                Delete
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </td>
+        </>
+    )
+}
