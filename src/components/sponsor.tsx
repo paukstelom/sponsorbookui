7c36e4374cc2897dbd2fdae3e31f5c2b864c72d2
paukstelom <paukstelom@gmail.com>
2023-06-18 23:16:53 +0300

    bruh

--- src/components/sponsor.tsx
+++ src/components/sponsor.tsx
@@ -29,57 +29,5 @@ export default function SponsorLine({ sponsor }: SponsorCardProps) {
         router.refresh()
     }
 
-    return (
-        <>
-            <td className="text-center">{sponsor.name}</td>
-            <td className="text-center">{sponsor.category}</td>
-            <td className="text-center">
-                <span className="badge badge-success rounded-pill d-inline bg-success">
-                    Available
-                </span>
-            </td>
-            <td className="text-center">{sponsor.contacts[0].email}</td>
-            <td className="text-center">{sponsor.contacts[0].phone}</td>
-            <td className="text-center">🔥🔥🔥💩💩</td>
-            <td className="text-align-right">
-                <div className="dropdown">
-                    <button
-                        className="btn dropdown-toggle"
-                        type="button"
-                        data-bs-toggle="dropdown"
-                        aria-expanded="false"
-                    ></button>
-                    <ul className="dropdown-menu">
-                        <li>
-                            <a className="dropdown-item" href="#">
-                                Connect
-                            </a>
-                        </li>
-                        <li>
-                            <Link
-                                className="dropdown-item"
-                                href={`/sponsors/${sponsor._id}`}
-                            >
-                                View
-                            </Link>
-                        </li>
-                        <li>
-                            <a className="dropdown-item" href="#">
-                                Edit
-                            </a>
-                        </li>
-                        <li>
-                            <button
-                                className="dropdown-item"
-                                type="submit"
-                                onClick={onSubmit}
-                            >
-                                Delete
-                            </button>
-                        </li>
-                    </ul>
-                </div>
-            </td>
-        </>
-    )
+    return <></>
 }
